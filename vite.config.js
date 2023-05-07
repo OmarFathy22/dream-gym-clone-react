7f605d157d413ca0f6f9949a839db70d24245501
morata <morata@gmail.com>
2023-05-08 01:27:23 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,14 @@
 import { defineConfig } from 'vite';
 import compression from 'vite-plugin-compression';
 import react from '@vitejs/plugin-react'
-import terser from '@rollup/plugin-terser';
-
 export default defineConfig({
   plugins: [
     react(),
-    // use the following for production build
     compression({
       algorithm: 'gzip',
       ext: '.gz',
       deleteOriginFile: true,
     }),
-    terser()
   ],
 });
 
