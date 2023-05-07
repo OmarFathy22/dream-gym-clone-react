7101443b840dffdab0193cf5124bbd73c1fe4c5a
morata <morata@gmail.com>
2023-05-08 01:15:10 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite';
 import compression from 'vite-plugin-compression';
 import react from '@vitejs/plugin-react'
-
+import terser from '@rollup/plugin-terser';
 
 export default defineConfig({
   plugins: [
     react(),
+    // use the following for production build
     compression({
+      algorithm: 'gzip',
       ext: '.gz',
+      deleteOriginFile: true,
     }),
+    terser()
   ],
 });
 
