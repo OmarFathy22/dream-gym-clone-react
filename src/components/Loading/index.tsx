ceeb39e154fdfd11ee5e0130f6da0cc4552fdaec
morata <morata@gmail.com>
2023-05-08 02:06:46 +0200

    new

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import './style.css'
 type Props = {}
 
 const Loading = (props: Props) => {
   return (
     <div>
       <main className='mainLoader flex justify-center items-center'>
-      <div className="loader"></div>
+      <div className="container">
+  <div className="loading">
+    <div className="loading__letter">L</div>
+    <div className="loading__letter">o</div>
+    <div className="loading__letter">a</div>
+    <div className="loading__letter">d</div>
+    <div className="loading__letter">i</div>
+    <div className="loading__letter">n</div>
+    <div className="loading__letter">g</div>
+    <div className="loading__letter">.</div>
+    <div className="loading__letter">.</div>
+    <div className="loading__letter">.</div>
+  </div>
+</div>
+
       </main>
     </div>
   )
