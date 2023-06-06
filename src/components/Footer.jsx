584a13e72979b9f03d3dac54431fdd84b6a41eda
Omar Fathy <91073589+OmarFathy22@users.noreply.github.com>
2023-06-06 04:11:36 +0300

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <footer className='bg-white flex justify-center items-center mt-10   gap-3 py-5 rounded-md border-t-2 border-t-red-500 flex-col '>
         <div className='flex gap-2 items-end'>
           <img height={30} width={30} src={logo} alt='icon' />
-          <GymName style={{ fontFamily: 'Abril Fatface, cursive', textTransform: 'lowercase' }} className='morata'>Your Way to Forma👀</GymName>
+          <GymName style={{ fontFamily: 'Abril Fatface', textTransform: 'lowercase' }} className='morata'> Way to Forma👀</GymName>
         </div>
         <div>
           <h3 style={{ fontFamily: 'Orbitron ,sans-serif,Abril Fatface , cursive' , fontWeight: '400' , color: "#f07408" }}>Made with ❤️ by Omar Fathy</h3>
--- src/components/ForTesting.jsx
+++ src/components/ForTesting.jsx
@@ -12,6 +12,14 @@ import { EffectCards } from "swiper";
 import "swiper/swiper-bundle.min.css";
 import { EXERCISENAME } from "./features/exerciseName";
 import { useDispatch } from "react-redux";
+import chest from '../assets/exercises/chest.png'
+import shoulder from '../assets/exercises/shoulder.png'
+import arms from '../assets/exercises/arms.png'
+import back from '../assets/exercises/back.jpg'
+import legs from '../assets/exercises/legs.jpg'
+import waist from '../assets/exercises/waist.jpg'
+import cardio from '../assets/exercises/cardio.jpeg'
+import neck from '../assets/exercises/neck.jfif'
 
 const ForTesting = () => {
   const [Value] = useDocument(
@@ -33,17 +41,46 @@ const ForTesting = () => {
         modules={[EffectCards]}
         className="mySwiper-1 justify-center items-center flex"
       >
-        {Value?.data()?.firstArray?.map((exercise, index) => {
+        {Value?.data()?.firstArray?.map((exercise, index) => { 
+          let exerciseImage = '';
+          if(exercise === 'chest'){
+            exerciseImage = chest;
+          }
+          if(exercise === 'back'){
+            exerciseImage = back;
+          }
+          if(exercise === 'shoulders'){
+            exerciseImage = shoulder;
+          }
+          if(exercise === 'neck'){
+            exerciseImage = neck;
+          }
+          if(exercise === 'waist'){
+            exerciseImage = waist;
+          }
+          if(exercise === 'cardio'){
+            exerciseImage = cardio;
+          }
+          if(exercise === 'upper arms' || exercise === 'lower arms'){
+            exerciseImage = arms;
+            exercise = 'arms'
+          }
+          if(exercise === 'upper legs' || exercise === 'lower legs'){
+            exerciseImage = legs;
+            exercise = 'legs'
+          }
+      
           return (
             <SwiperSlide
               onClick={() => {
                 handleClickScroll()
                 dispatch(EXERCISENAME(exercise));
               }}
-              className="swiper-slide-1 !flex !justify-center !items-center"
+              className="swiper-slide-1 !flex !justify-center !items-center relative"
               key={index}
             >
-              <h1>{exercise}</h1>
+              <h1 className="absolute text-white text-[45px]">{exercise}</h1>
+              <img src={exerciseImage} alt="image" className="object-cover h-full w-full opacity-[0.3]"/>
             </SwiperSlide>
           );
         })}
